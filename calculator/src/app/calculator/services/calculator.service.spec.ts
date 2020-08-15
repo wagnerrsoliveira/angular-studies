1334df607d2bc0c6a023754f050af7aebf567d72
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-15 16:11:37 -0300

    angular-seven-projects: caculator - testing method calculate on the calculator service

--- calculator/src/app/calculator/services/calculator.service.spec.ts
+++ calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
@@ -13,4 +13,40 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should ensure that 1 + 4 = 5',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let addition = service.calculate(1, 4, CalculatorService.ADDITION);
+      expect(addition).toEqual(5);
+    })
+  );
+
+  it('should ensure that 1 - 4 = -3',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let subtraction = service.calculate(1, 4, CalculatorService.SUBTRACTION);
+      expect(subtraction).toEqual(-3);
+    })
+  );
+
+  it('should ensure that 1 / 4 = 0.25',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let division = service.calculate(1, 4, CalculatorService.DIVISION);
+      expect(division).toEqual(0.25);
+    })
+  );
+
+  it('should ensure that 1 * 4 = 4',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let multiplication = service.calculate(1, 4, CalculatorService.MULTIPLICATION);
+      expect(multiplication).toEqual(4);
+    })
+  );
+
+  it('should return 0 when receive invalid operator',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let invalidOperator = service.calculate(1, 4, '%');
+      expect(invalidOperator).toEqual(0);
+    })
+  );
+
 });
