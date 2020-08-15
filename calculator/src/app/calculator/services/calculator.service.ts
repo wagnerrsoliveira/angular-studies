2f7c0cabdafce4cda582766b14e728713c7a7608
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-15 15:43:58 -0300

    angular-seven-projects: caculator - implementation of the method calculate on the calculator serice

--- calculator/src/app/calculator/services/calculator.service.ts
+++ calculator/src/app/calculator/services/calculator.service.ts
@@ -5,5 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
 
+  static readonly ADDITION: string = '+';
+  static readonly SUBTRACTION: string = '-';
+  static readonly DIVISION: string = '/';
+  static readonly MULTIPLICATION: string = '*';
+  
   constructor() { }
+
+  calculate(firstNumber:number,secondNumber:number, operator:string): number{
+    let result:number;
+
+    switch (operator) {
+      case CalculatorService.ADDITION:
+        result = firstNumber + secondNumber;        
+        break;
+      case CalculatorService.SUBTRACTION:
+        result = firstNumber - secondNumber;        
+        break;
+      case CalculatorService.DIVISION:
+        result = firstNumber / secondNumber;
+        break;
+      case CalculatorService.MULTIPLICATION:
+        result = firstNumber * secondNumber;        
+        break;    
+      default:
+        result = 0;
+    }
+
+    return result;
+  }
 }
