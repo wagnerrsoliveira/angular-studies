602f7a59829feb934022cdf6a501c58b3d42c2d5
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-16 08:27:13 -0300

    angular-seven-projects: caculator - methods  for calculate on the calculator component

--- calculator/src/app/calculator/components/calculator.component.ts
+++ calculator/src/app/calculator/components/calculator.component.ts
@@ -9,9 +9,88 @@ import { CalculatorService } from '../services'
 })
 export class CalculatorComponent implements OnInit {
 
+  private firstNumber: string;
+  private secondNumber: string;
+  private result: number;
+  private operation: string;
+
   constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit(): void {
+    this.clear();
+  }
+
+  clear(): void {
+    this.firstNumber = '0';
+    this.secondNumber = null;
+    this.result = null;
+    this.operation = null;
+  }
+
+  addNumber(number:string){
+    if(this.operation===null){
+      this.firstNumber = this.concatenateNumber(this.firstNumber,number);
+    }else{
+      this.secondNumber = this.concatenateNumber(this.secondNumber,number);
+    }
+  }
+
+  concatenateNumber(currentNumber: string, concatenatorNumber: string): string {
+    
+    if(currentNumber === '0' || currentNumber ===null){
+      return '0.';
+    }
+
+
+    if(concatenatorNumber === '.' && currentNumber === ''){
+      return '0.';
+    }
+
+
+    if(concatenatorNumber === '.' || currentNumber.indexOf('.') > -1){
+      return currentNumber;
+    }
+
+    return currentNumber + concatenatorNumber;
+  }
+
+  defineOperation(operation:string):void {
+    if(this.operation!== null){
+      this.operation = operation;
+      return;
+    }
+
+    if(this.secondNumber !== null){
+      this.result = this.calculatorService.calculate(
+        parseFloat(this.firstNumber),
+        parseFloat(this.secondNumber),
+        this.operation);
+      this.operation= operation;
+      this.firstNumber = this.result.toString();
+      this.secondNumber = null;
+      this.result= null;
+    }
+  }
+
+  calculate():void {
+    if(this.secondNumber===null){
+      return;
+    }
+
+    this.result =  this.result = this.calculatorService.calculate(
+      parseFloat(this.firstNumber),
+      parseFloat(this.secondNumber),
+      this.operation);
+  }
+
+  get display():string {
+    if(this.result  !== null){
+      return this.result.toString();
+    }
+    if(this.secondNumber !== null){
+      return this.secondNumber;
+    }
+    return this.firstNumber;
   }
 
 }
