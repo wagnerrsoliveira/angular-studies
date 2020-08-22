06b3cb5ebbeb310b7d068bf786601c8bba6e13dc
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-22 10:57:51 -0300

    angular-seven-projects: task manager - created methods getById, update, delete and updateStatus inside task service

--- task-manager/src/app/tasks/shared/task.service.ts
+++ task-manager/src/app/tasks/shared/task.service.ts
@@ -20,4 +20,37 @@ export class TaskService {
     tasks.push(task);
     localStorage['tasks'] = JSON.stringify(tasks)
   }
+
+  getById(id:number):Task{
+    const tasks = this.getAll();
+    return tasks.find(task => task.id === id);
+  }  
+
+  update(task:Task):void{
+    const tasks =this.getAll();
+    tasks.forEach((obj,index,objs)=>{
+      if(task.id === obj.id){
+        objs[index] = task;
+      }
+    });
+    localStorage['tasks'] = JSON.stringify(tasks);
+  }
+
+  delete(id:number):void{
+    let tasks = this.getAll();
+    tasks = tasks.filter(task => task.id !== id);
+    localStorage['tasks'] =  JSON.stringify(tasks);
+  }
+
+  updateStatus(id:number):void{
+    const tasks = this.getAll();
+    tasks.forEach((obj, index, objs)=>{
+      if(id === obj.id){
+        objs[index].done = !obj.done;
+      }
+    });
+    localStorage['tasks'] = JSON.stringify(tasks);
+  }
+
+
 } 
