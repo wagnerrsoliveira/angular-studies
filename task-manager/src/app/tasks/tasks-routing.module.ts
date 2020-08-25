6fc5fe7fd1cbd77e888d0b15e5e588456e54d593
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-25 06:28:49 -0300

    angular-seven-projects: task manager - registered task add

--- task-manager/src/app/tasks/tasks-routing.module.ts
+++ task-manager/src/app/tasks/tasks-routing.module.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { ListTasksComponent } from './list';
+import { AddTaskComponent } from './add';
 
 export const TaskRoutes: Routes = [
     {
@@ -10,5 +11,9 @@ export const TaskRoutes: Routes = [
     {
         path: 'tasks/list',
         component: ListTasksComponent
+    },
+    {
+        path: 'tasks/add',
+        component: AddTaskComponent
     }
 ];
