cd1dc1c9abdc2321b9ed879e331dfd9de9dd3fa2
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-27 06:31:54 -0300

    angular-seven-projects: task manager - implemented edit task

--- task-manager/src/app/tasks/edit/edit-task.component.ts
+++ task-manager/src/app/tasks/edit/edit-task.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Task, TaskService } from '../shared';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-task',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTaskComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTask') formTask: NgForm;
+  task:Task;
+
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.params['id'];
+    this.task = this.taskService.getById(id);
+  }
+
+  update():void{
+    if(this.formTask.form.valid){
+      this.taskService.update(this.task);
+      this.router.navigate(['/tasks']);
+    }
   }
 
 }
