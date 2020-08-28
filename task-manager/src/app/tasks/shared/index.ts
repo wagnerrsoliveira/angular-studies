d168619e243ffc47216aed620ce4374f350b9dda
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-28 06:32:30 -0300

    angular-seven-projects: task manager - created directive of task done

--- task-manager/src/app/tasks/shared/index.ts
+++ task-manager/src/app/tasks/shared/index.ts
@@ -1,2 +1,3 @@
 export * from './task.model';
 export * from './task.service';
+export * from  './task-done.directive'
--- task-manager/src/app/tasks/tasks.module.ts
+++ task-manager/src/app/tasks/tasks.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import {FormsModule } from  '@angular/forms';
 
-import { TaskService} from './shared';
+import { TaskService, TaskDoneDirective} from './shared';
 import { ListTasksComponent } from './list';
 import { AddTaskComponent } from './add';
 import { EditTaskComponent } from './edit';
 
 @NgModule({
-  declarations: [ListTasksComponent, AddTaskComponent, EditTaskComponent],
+  declarations: [ListTasksComponent, AddTaskComponent, EditTaskComponent, TaskDoneDirective],
   imports: [
     CommonModule,
     RouterModule,
