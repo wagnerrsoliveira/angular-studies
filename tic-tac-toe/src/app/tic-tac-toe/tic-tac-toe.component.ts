4227dc56b7b9c12f77e3c66ac69e06ab85b91c2c
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-29 07:36:00 -0300

    angular-seven-projects: tic tac toe - created component tic tac toe

---
+++ tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tic-tac-toe',
+  templateUrl: './tic-tac-toe.component.html',
+  styleUrls: ['./tic-tac-toe.component.css']
+})
+export class TicTacToeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TicTacToeComponent } from './tic-tac-toe.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TicTacToeComponent],
   imports: [
     CommonModule
+  ],
+  exports:[
+    TicTacToeComponent
   ]
 })
 export class TicTacToeModule { }
