0c42787e5bc91e85ffb20ccf502528d4a4cebf09
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-08-30 07:45:09 -0300

    angular-seven-projects: tic tac toe - implemented service tic tac toe first part

--- tic-tac-toe/src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
+++ tic-tac-toe/src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
@@ -5,5 +5,60 @@ import { Injectable } from '@angular/core';
 })
 export class TicTacToeService {
 
+  private readonly BOARD_SIZE: number = 3;
+  private readonly X: number = 1;
+  private readonly O: number = 2;
+  private readonly EMPTY: number = 0;
+
+  private board: Array<number[]>;
+  private numberOfMoves: number;
+  private vitory: Array<number>;
+
+  private _player: number;
+  private _showStart: boolean;
+  private _showBoard: boolean;
+  private _showEnd: boolean;
+
   constructor() { }
+
+  initialize(): void {
+    this._showStart = true;
+    this._showBoard = false;
+    this._showEnd = false;
+    this.numberOfMoves = 0;
+    this._player = this.X;
+    this.vitory = [];
+
+    this.mountBoard();
+  }
+
+
+  mountBoard(): void {
+    this.board = new Array<number[]>();
+    for (let i = 0; i < this.BOARD_SIZE; i++){
+      this.board.push([this.EMPTY,this.EMPTY,this.EMPTY]);
+    }
+  }
+
+  get shoWStart():boolean{
+    return this._showStart;
+  }
+
+  get showBoard():boolean{
+    return this._showBoard;
+  }
+
+  get showEnd():boolean{
+    return this._showEnd;
+  }
+
+  get player():number{
+    return this._player;
+  }
+
+  startGame():void{
+    this._showStart = false;
+    this._showBoard = true;
+  }
+
 }
