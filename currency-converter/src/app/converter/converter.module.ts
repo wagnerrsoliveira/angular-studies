6222dbd78eceee51dc280945b3f7afd93d4536db
wagnerrsoliveira <wagnerrsoliveira@gmail.com>
2020-09-07 07:28:43 -0300

    angular-seven-projects: currency converter - created conversion component

--- currency-converter/src/app/converter/converter.module.ts
+++ currency-converter/src/app/converter/converter.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConverterComponent } from './components';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ConverterComponent],
   imports: [
     CommonModule
+  ],
+  exports:[
+    ConverterComponent
   ]
 })
 export class ConverterModule { }
--- currency-converter/src/app/converter/index.ts
+++ currency-converter/src/app/converter/index.ts
@@ -1,2 +1,3 @@
 export * from './converter.module';
 export * from './models';
+export * from './components';
